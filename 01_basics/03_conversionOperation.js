1823d6e8da703533c62a44a3d61d19850994cfc3
Sundramkumari <sundramkumari432@gmail.com>
2025-01-12 14:33:15 +0000

    Comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,23 @@ let someNumber=33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//   *****************************************Operations******************************************** 
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+let str1="hello"
+let str2=" hitesh"
+let str3=str1+str2
+console.log(str3);
+
+// console.log(1+2+"2")
+// console.log("1"+2+3)  //not good practice
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
