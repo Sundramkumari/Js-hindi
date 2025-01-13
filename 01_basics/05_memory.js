9070b80b8542d192fa5fecbf136a69dfda6baa50
Sundramkumari <sundramkumari432@gmail.com>
2025-01-13 15:33:03 +0000

    memory types

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,20 @@
+//Types of memory
+//Stack(Primitive), Heap(Non-primitive)
+
+let myYoutubename="skpdotcom"
+let anothername=myYoutubename
+anothername="chaiandcode"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo=userOne
+userTwo.email="svmstym@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
