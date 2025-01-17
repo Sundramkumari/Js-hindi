974a618c77a385ce11c9b37dd1d01fcc109f798d
Sundramkumari <sundramkumari432@gmail.com>
2025-01-17 17:47:57 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,26 @@
+// var c = 300
+let a=300
+if(true){
+let a=10
+const b=20
+// var c=30
+console.log("INNER: ", a);
+}
+
+// {} => for function or if else it is called as scope
+
+
+//SYNTEX of for loop
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+
+console.log(a);
+//console.log(b);
+// console.log(c);
+
+/* IMPORTANT:::::the global scope is very different either we are using console to print the output or
+                 we are using node to print the output..  */
